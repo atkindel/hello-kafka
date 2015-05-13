0cba5187f67ece93abb1aa501ebeb87922baa1ce
Alex Kindel <atkindel@stanford.edu>
2015-05-13 14:24:03 -0700

    Hello world for simple kafka-python messaging

--- src/hello-kafka/kafkaconsumer.py
+++ src/hello-kafka/kafkaconsumer.py
@@ -1,1 +1,13 @@
-pass
+from kafka import SimpleConsumer, KafkaClient
+
+# Consumer listens for Kafka messages on port 9092.
+k = KafkaClient('localhost:9092')
+
+# User provides name of event log (could be producer module name, e.g.)
+title = str(raw_input("Which event log to listen to?: "))
+
+# Consumer prints out all events logged by any producer. Times out in 3 minutes.
+log = SimpleConsumer(k, group=None, topic=title, iter_timeout=180)
+for event in log:
+    msg = event.message.value
+    print msg
--- src/hello-kafka/kafkaproducer.py
+++ src/hello-kafka/kafkaproducer.py
@@ -1,1 +1,23 @@
-pass
+from kafka import SimpleProducer, KafkaClient
+from random import randint
+
+# Producer module sends out Kafka messages on port 9092
+k = KafkaClient("localhost:9092")
+producer = SimpleProducer(k)
+
+# User specifies name for event log (e.g. name of module or activity)
+title = str(raw_input("Name event log: "))
+k.ensure_topic_exists(title)
+
+# Unique user sends messages as needed.
+uid = "Learner" + str(randint(0, 20000))
+while True:
+    event = raw_input("Add what to event log?: ('Q' to end.): ")
+    if event == 'Q':
+        break
+    else:
+        msg = event.encode('UTF-8', 'ignore')
+        producer.send_messages(title, "%s: %s" % (uid, msg))
+
+# Module closes connection on exit.
+k.close()
